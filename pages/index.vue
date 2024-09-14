7548eb6268a26c8e9434abf524e4ca6406153d61
Adem <37814393+floki1250@users.noreply.github.com>
2024-09-14 12:25:06 +0100

    Testing

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,31 @@
     <ion-page>
         <ion-content>
             <p>Index Page</p>
+            <ion-button @click="checkNfcStatus">Check NFC</ion-button>
+            <p>{{ status }}</p>
         </ion-content>
     </ion-page>
 </template>
+<script setup>
+import { NFC } from 'capacitor-nfc';
+const status = ref('')
+const nfc = new NFC();
+function checkNfcStatus () {
+    nfc.checkNfcEnabled().then(enabled => {
+        if (enabled) {
+            // NFC is enabled
+            status.value = "enabled"
+            nfc.requestNfcPermission().then(permission => {
+                if (permission === 'granted') {
+                    // NFC permission granted
+                    status.value = status.value + ', permission granted'
+                    nfc.addNfcTagDiscoveredListener().subscribe(tag => {
+                        // Tag discovered, handle it
+                        console.log(tag);
+                    });
+                }
+            });
+        }
+    });
+}
+</script>
